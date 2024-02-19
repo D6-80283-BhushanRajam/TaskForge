20bed75058ac5652595d5bb1c6c28e1b1ca29970
D6-80714-SushilSamant <144477073+D6-80714-SushilSamant@users.noreply.github.com>
2024-02-20 00:55:24 +0530

    Updated All Page Components
    Footer - SocialMedia LogoLinks added,
    Updated About Us and Contact Us Pages.

--- Frontend/task-management-system-frontend-master/src/page/AboutUs.jsx
+++ Frontend/task-management-system-frontend-master/src/page/AboutUs.jsx
@@ -1,59 +1,133 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const styles = {
+  body: {
+    margin: 0,
+    fontFamily: 'Arial, sans-serif',
+    
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover', 
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
+  aboutUsContainer: {
+    textAlign: 'center',
+    padding: '20px',
+    backgroundColor:'	#F8F8F8',
+    borderRadius: '10px',
+    
+    width: '80%', 
+    maxWidth: '600px', 
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+  },
+  nav: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: '5px',
+    backgroundColor: '#4399e9',
+    width: '100%',
+  },
+  navLinks: {
+    listStyle: 'none',
+    display: 'flex',
+    gap: '30px',
+  },
+  navLinksItem: {
+    marginTop:'10px',
+    fontFamily: "Comic Sans MS, Comic Sans, cursive",
+    fontSize: '20px',
+    color: 'white',
+    alignItems: 'center',
+    
+  },
+  navLinksLink: {
+    textDecoration: 'underline',
+    color: 'white',
+  },
+  aboutUsContent: {
+    fontFamily: "Comic Sans MS, Comic Sans, cursive",
+    marginTop: '30px',
+    color: 'black',
+  },
+  teamList: {
+    fontFamily: "Comic Sans MS, Comic Sans, cursive",
+    listStyle: 'none',
+    padding: 0,
+    marginTop: '20px',
+  },
+  teamMember: {
+    fontFamily: "Comic Sans MS, Comic Sans, cursive",
+    marginBottom: '15px',
+    padding: '15px',
+    border: '1px solid #0E0E0C',
+    borderRadius: '8px',
+  },
+  teamMemberName: {
+    fontFamily: "Comic Sans MS, Comic Sans, cursive",
+    fontSize: '20px',
+    fontWeight: 'bold',
+    margin: '10px 0',
+  },
+  teamMemberRole: {
+    fontFamily: "Times New Roman, Times, serif",
+    fontSize: '16px',
+    color: '#555',
+  }
+  
+};
 const AboutUs = () => {
-  return (
-    <div className="text-color ms-5 me-5 mr-5 mt-3">
-      <b>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-        error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
-        modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
-        error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
-        dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed
-        amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
-        temporibus enim commodi iusto libero magni deleniti quod quam
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad
-        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores
-        labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
-        ab laudantium modi minima sunt esse temporibus sint culpa, recusandae
-        aliquam numquam totam ratione voluptas quod exercitationem fuga.
-        Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
-        1427 lorem ipsumWhatever By Shadow on Mar 26 2022 DonateThankComment
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-        error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
-        modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
-        error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
-        dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed
-        amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
-        temporibus enim commodi iusto libero magni deleniti quod quam
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad
-        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores
-        labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto
-        ab laudantium modi minima sunt esse temporibus sint culpa, recusandae
-        aliquam numquam totam ratione voluptas quod exercitationem fuga.
-        Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!
-        1427 lorem ipsumWhatever By Shadow on Mar 26 2022 DonateThankComment
-      </b>
+  return (<div className='d-flex justify-content-center align-items-center vh-100 loginPage '>
+  <div style={styles.body}>
+  
+ 
+          
+    <div style={styles.aboutUsContainer}>
+    
+      <header>
+        <nav style={styles.nav}>
+          <ul style={styles.navLinks} className="nav-links">
+            <li style={styles.navLinksItem}><Link style={styles.navLinksLink} to="/">Home</Link></li>
+           
+          </ul>
+        </nav>
+      </header>
+      
+      <section style={styles.aboutUsContent} className="about-us-content">
+      
+        <h1>About Us</h1>
+
+        <p>We are a team of dedicated developers passionate about creating innovative solutions.</p>
+
+        <h2>Meet the Team:</h2>
+        <ul style={styles.teamList}>
+          <li style={styles.teamMember}>
+            <div style={styles.teamMemberName}><h5>Sushil Samant</h5>
+            <h5>Bhushan Rajam</h5></div>
+            <div style={styles.teamMemberRole}>Frontend Developer</div>
+          </li>
+          <li style={styles.teamMember}>
+            <div style={styles.teamMemberName}><h5>Vaibhav Tade</h5>
+            <h5>Rahul Neekhare</h5></div>
+            <div style={styles.teamMemberRole}>Backend Developer</div>
+          </li>
+       
+        </ul>
+
+        <p>Feel free to contact us if you have any questions or feedback.</p>
+      </section>
+
+     
     </div>
+    
+  </div>
+
+  </div>
+
   );
 };
 
